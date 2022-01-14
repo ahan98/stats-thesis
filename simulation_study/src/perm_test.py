e756e13dc149e9350205c9d10f76e2660b5a47e8
Alex Han <alex@Alexs-Air.lan>
2022-01-14 16:40:27 -0500

    cleanup

--- simulation_study/src/perm_test.py
+++ simulation_study/src/perm_test.py
@@ -1,5 +1,14 @@
 import numpy as np
+from scipy.stats import ttest_ind
 import statsmodels.stats.api as sms
+
+
+def permInterval(x1, x2, partitions, delta_true, pooled=True, alpha=0.05, alternative="two-sided"):
+    wide_lo, wide_hi = tconf(x1, x2, pooled=pooled, alpha=0.01)
+    narrow_lo, narrow_hi = tconf(x1, x2, pooled=pooled, alpha=0.1)
+    lo = search(x1, x2, partitions, wide_lo, narrow_lo, alpha=alpha, pooled=pooled, alternative=alternative)
+    hi = search(x1, x2, partitions, narrow_hi, wide_hi, alpha=alpha, pooled=pooled, alternative=alternative)
+    return lo <= delta_true <= hi
 
 
 def search(x1, x2, partitions, start, end, margin=0.005, threshold=1,
@@ -51,16 +60,14 @@ def search(x1, x2, partitions, start, end, margin=0.005, threshold=1,
         p_new = pval(x1, x2, partitions, delta=delta, pooled=pooled, alternative=alternative)
         # print("p_new =", p_new)
         if p and percent_change(p, p_new) <= threshold:
-            # (1) percent change is below threshold
-            break
+            break  # (1) percent change is below threshold
 
         if p_new > alpha + margin:
             start = delta
         elif p_new < alpha - margin:
             end = delta
         else:
-            # (2) p-value is within margin of error
-            break
+            break  # (2) p-value is within margin of error
 
         p = p_new
         i += 1
@@ -80,51 +87,26 @@ def pval(x1, x2, partitions, delta=0, pooled=True, alternative="two-sided"):
     x2 : nd.array
         Data for group 2
     """
-    n1, n2 = len(x1), len(x2)
-    #print(n1, n2)
-    # In general, due to this shift, the observed test statistic is not constant,
-    # even though the observed data (prior to the shift) remains the same.
     x1 = x1 - delta
+    t_obs = ttest_ind(x1, x2, axis=-1, equal_var=pooled, alternative=alternative).statistic
 
     combined = np.append(x1, x2)
+    n1 = len(x1)
     x1s = combined[partitions[:,:n1]]
     x2s = combined[partitions[:,n1:]]
-    ts = ttest_ind(x1s, x2s, n1, n2, pooled)
-    t_obs = ttest_ind(x1, x2, n1, n2, pooled)
+    ts = ttest_ind(x1s, x2s, axis=-1, equal_var=pooled, alternative=alternative).statistic
 
-    if alternative == "smaller":
+    if alternative == "less":
         subset = np.where(ts <= t_obs)
-    elif alternative == "larger":
+    elif alternative == "greater":
         subset = np.where(ts >= t_obs)
     else:
         subset = np.where((ts <= -abs(t_obs)) | (ts >= abs(t_obs)))
 
-    p = len(subset[0]) / len(partitions)
-    return p
+    return len(subset[0]) / len(partitions)
 
 
-# TODO documentation
-def ttest_ind(x1s, x2s, n1, n2, pooled=True):
-    #n1, n2 = x1s.shape[-1], x2s.shape[-1]
-    #print("n1 =", n1, "n2 =", n2)
-    sum1 = np.sum(x1s, axis=-1)
-    sum2 = np.sum(x2s, axis=-1)
-    #print("sums", sum1, sum2)
-
-    mean1 = sum1 / n1
-    mean2 = sum2 / n2
-    #print("means", mean1, mean2)
-
-    var1 = np.var(x1s, ddof=1, axis=-1)
-    var2 = np.var(x2s, ddof=1, axis=-1)
-    #print("sample variances", var1, var2)
-
-    # http://www.stat.yale.edu/Courses/1997-98/101/meancomp.htm
-    if pooled:
-        pooled_var = ((n1-1)*var1 + (n2-1)*var2) / (n1+n2-2)
-        denom = np.sqrt(pooled_var * (1/n1 + 1/n2))
-    else:
-        denom = np.sqrt(var1/n1 + var2/n2)
-
-    t = (mean1 - mean2) / denom
-    return t
+# https://www.statsmodels.org/dev/generated/statsmodels.stats.weightstats.CompareMeans.tconfint_diff.html
+def tconf(x1, x2, alpha=0.05, pooled=True, alternative="two-sided"):
+    cm = sms.CompareMeans(sms.DescrStatsW(x1), sms.DescrStatsW(x2))
+    return cm.tconfint_diff(alpha, alternative, usevar="pooled" if pooled else "unequal")
