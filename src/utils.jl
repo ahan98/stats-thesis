62efc15eebca35b6b885536d7906db3dcb7338d7
ahan98 <ahan98@gmail.com>
2022-02-06 07:55:07 +0000

    delete unused function

--- src/utils.jl
+++ src/utils.jl
@@ -6,8 +6,3 @@ function partition(n1, n2)
     b = hcat(combinations(1:N, n2)...)'
     return a, reverse(b)
 end
-
-function set_thread_block(len, nthreads=256)
-    nblocks = ceil(Int, len / nthreads)
-    return nthreads, nblocks
-end
