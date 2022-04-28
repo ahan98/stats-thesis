677e8dd0182616e8fcb3b81fe13c1755f2bbf680
Alex Han <ahan98@gmail.com>
2022-04-28 16:13:39 +0000

    sim 1 imbalanced sizes (15, 8)

--- src/intervals/bootstrap.jl
+++ src/intervals/bootstrap.jl
@@ -1,24 +1,29 @@
 using StatsBase
 using FLoops
 
-function bootstrap(x, y, delta, alpha, pooled=false; nsamples=10_000)
-    lo, hi = bootstrap(x, y, alpha, pooled, nsamples)
+function bootstrap(x, y, delta, alpha; nsamples=2000)
+    lo, hi = bootstrap(x, y, alpha, nsamples)
     return lo <= delta <= hi, hi - lo
 end
 
-function bootstrap(x, y, alpha, pooled, nsamples, nx=length(x), ny=length(y))
-    sample_stats = zeros(nsamples)
+function bootstrap(x, y, alpha, nsamples)
+    stats = zeros(nsamples)
     T = Threads.nthreads()
+    nx, ny = length(x), length(y)
     @floop ThreadedEx(basesize=div(nsamples, T)) for i in 1:nsamples
-        x_bs = StatsBase.sample(x, nx)
-        y_bs = StatsBase.sample(y, ny)
-        @inbounds sample_stats[i] = mean(x_bs) - mean(y_bs)#t(x_bs, y_bs, pooled)[1]
+        @views xx = sample(x, nx, replace=true)
+        @views yy = sample(y, ny, replace=true)
+        @inbounds stats[i] = mean(xx) - mean(yy)
     end
-    # sort!(sample_stats)
+    
     tail_size = ceil(Int, nsamples * alpha / 2)
-    # return sample_stats[tail_size + 1], sample_stats[end-tail_size]
-    lower = findKthLargest(nums, tail_size + 1)
-    upper = findKthLargest(nums, nsamples - tail_size)
+    
+    # sort!(sample_stats)
+    # lower = sample_stats[tail_size + 1]
+    # upper = sample_stats[end-tail_size]
+    
+    lower = findKthLargest(stats, tail_size + 1)
+    upper = findKthLargest(stats, nsamples - tail_size)
     return lower, upper
 end
 
@@ -28,7 +33,7 @@ function findKthLargest(nums, k)
 
     while true
         pivot_idx = rand(lo:hi)
-        pivot_idx = partition(pivot_idx, lo, hi)
+        pivot_idx = partition(nums, pivot_idx, lo, hi)
 
         if pivot_idx == k
             return nums[k]
@@ -43,7 +48,7 @@ function findKthLargest(nums, k)
 end
 
 
-function partition(pivot_idx, lo, hi)
+function partition(nums, pivot_idx, lo, hi)
     pivot_val = nums[pivot_idx]
 
     nums[hi], nums[pivot_idx] = nums[pivot_idx], nums[hi]
